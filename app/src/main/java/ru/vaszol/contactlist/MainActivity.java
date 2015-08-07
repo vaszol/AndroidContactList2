70433e98830a818d57429c9051a440ffc2fa74a1
vas <vaszol@mail.ru>
2015-08-07 12:52:54 +0300

    реализовал вывод списка объектов, через модель контактов и адаптер контактов

--- app/src/main/java/ru/vaszol/contactlist/MainActivity.java
+++ app/src/main/java/ru/vaszol/contactlist/MainActivity.java
@@ -10,6 +10,9 @@ import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.List;
 
+import ru.vaszol.contactlist.adapter.ContactModelAdapter;
+import ru.vaszol.contactlist.pojo.ContactModel;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -22,16 +25,17 @@ public class MainActivity extends ActionBarActivity {
 
         listView= (ListView) findViewById(R.id.listView);
 
-        List<String> items = initData();
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1, items);
+//        List<String> items = initData();
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1, items);
+        ContactModelAdapter adapter =new ContactModelAdapter(this,initData());
         listView.setAdapter(adapter);
     }
 
-    private List<String> initData(){
-        List<String> list = new ArrayList<String>();
-        list.add("Ivanov");
-        list.add("Petrov");
-        list.add("Sidorov");
+    private List<ContactModel> initData(){
+        List<ContactModel> list = new ArrayList<ContactModel>();
+        list.add(new ContactModel(1,"Ivan","Ivanov"));
+        list.add(new ContactModel(1,"Petr","Petrov"));
+        list.add(new ContactModel(1,"Alex","Sidorov"));
         return list;
 
     }
